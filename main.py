db6f2e073aebda315a7d9408f435a89527a4ba1b
Ryan Munin <ryan.munin@gmail.com>
2025-12-06 18:19:56 -0800

    file read handled

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 def main():
-    print("Hello World!")
+    raw_list = readFile()
+
+# Read the CSV file input
+def readFile():
+    in_file = open("TeamList.csv")
+    raw_list = []
+
+    for line in in_file:
+        raw_list.append(line.split(", "))
+
+    return raw_list
+
+# Take the input from the file and process it into the arrays that will be output
+def calculateValues(inputArray):
+    pass
+
+# generate the random values.
+def generateRandom():
+    pass
+
+# actually pick the team given the random input
+def pickTeam(teams, ranges, rand):
+    pass
 
 main()
